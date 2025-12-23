a8a7d5bbfb8c0527a94fb0030fbf9c69a461a270
Rangerbiru23 <rangerbiru1@outlook.co.id>
2025-12-23 20:43:04 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="30ai2a79yy16gbe282tp1st40qguqb" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
